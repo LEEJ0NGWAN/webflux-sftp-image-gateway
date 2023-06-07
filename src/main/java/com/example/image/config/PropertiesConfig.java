2760ea61921ae29ba1d9e3b872433625389c1d85
LEEJ0NGWAN <lastffx@gmail.com>
2023-06-07 22:58:57 +0900

    refactor: properties configuration

--- src/main/java/com/example/image/config/PropertiesConfig.java
+++ src/main/java/com/example/image/config/PropertiesConfig.java
@@ -1,10 +1,8 @@
 package com.example.image.config;
 
-import com.example.image.properties.SftpProperties;
-
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@EnableConfigurationProperties({ SftpProperties.class })
+@ConfigurationPropertiesScan(basePackages = "com.example.image.properties")
 public class PropertiesConfig {}
